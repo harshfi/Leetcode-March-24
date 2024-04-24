0af0a83925493e42b50e6baa1cbf6882a8d37e2a
harshfi <86000141+harshfi@users.noreply.github.com>
2024-04-24 11:17:10 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (92.34%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -4,9 +4,7 @@ class Solution {
         int t0=0;
         int t1=1;
         int t2=1;
-        if(n==0)return t0;
-        if(n==1)return t1;
-     
+       
 
         for(int i=3;i<=n;i++){
             int temp1=t0+t1+t2;
@@ -14,7 +12,7 @@ class Solution {
             t1=t2;
             t2=temp1;
         }
-        return t2;
+        return n==0?t0:n==1?t1:t2;
         
     }
 }
