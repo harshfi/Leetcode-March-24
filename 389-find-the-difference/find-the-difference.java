8f0064b7e4d7e2fbdc77bebfe835ec3083606cfb
harshfi <86000141+harshfi@users.noreply.github.com>
2024-06-09 00:54:19 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (7.50%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,18 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int a[]=new int[26];
-        int b[]=new int [26];
-
-        for(int i=0;i<s.length();i++){
-            a[(int)(s.charAt(i)-'a')]++;
-            b[(int)(t.charAt(i)-'a')]++;
+       char c = 0;
+        s += t;
+        for(char ch: s.toCharArray()) {
+            c ^= ch;
         }
-        b[(int)(t.charAt(s.length())-'a')]++;
-
-        for(int i=0;i<26;i++){
-            if(a[i]!=b[i]) return (char)(i+97);
-        }
-        return ' ';
+        return c;
         
     }
 }
