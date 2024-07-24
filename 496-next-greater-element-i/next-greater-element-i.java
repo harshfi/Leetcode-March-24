23d3e70c8007c378b085fd18397bc555d2acfbd0
harshfi <86000141+harshfi@users.noreply.github.com>
2024-07-24 13:11:28 +0530

    Time: 7 ms (8.24%) | Memory: 43.2 MB (75.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st=new Stack<>();
-        int ng[]=new int[nums2.length];
+        Stack<Integer> st= new Stack<>();
+        int ng[]= new int [nums2.length];
+
         for(int i=0;i<nums2.length;i++){
-            while(!st.isEmpty() &&nums2[st.peek()]<nums2[i]){
+            while(!st.isEmpty() && nums2[st.peek()]<nums2[i]){
                 int idx=st.pop();
                 ng[idx]=nums2[i];
             }
             st.push(i);
+
         }
         while(!st.isEmpty()){
             int idx=st.pop();
-            ng[idx]=-1;
+                ng[idx]=-1;
         }
         int ans[]=new int [nums1.length];
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
-                  ans[i]=ng[j];
+                    ans[i]=ng[j];
                 }
             }
         }
         return ans;
-
-       
+        
+        
     }
 }
