b2b16741efd0f0c2fca36b65b090ad1e3e1898e0
harshfi <86000141+harshfi@users.noreply.github.com>
2024-07-24 11:46:49 +0530

    Time: 2 ms (58.60%) | Memory: 41.6 MB (47.91%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> st1= new Stack<>();
-         Stack<Character> st2= new Stack<>();
+        Stack<Character> st1=new Stack<>();
+        Stack<Character> st2=new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='#' && !st1.isEmpty() ){
-                st1.pop();
-            }
-            else if(s.charAt(i)!='#')st1.push(s.charAt(i));
+            char ch=s.charAt(i);
+            if(ch=='#' && !st1.isEmpty())st1.pop();
+           else if(ch!='#')st1.push(ch);
         }
         for(int i=0;i<t.length();i++){
-            if(t.charAt(i)=='#' && !st2.isEmpty() ){
-                st2.pop();
-            }
-            else if(t.charAt(i)!='#')st2.push(t.charAt(i));
+            char ch=t.charAt(i);
+            if(ch=='#' && !st2.isEmpty())st2.pop();
+            else if(ch!='#') st2.push(ch);
         }
-        if(st1.size()!=st2.size()) return false;
-
+        if(st1.size()!=st2.size())return false;
         while(!st1.isEmpty()){
             char ch1=st1.pop();
             char ch2=st2.pop();
