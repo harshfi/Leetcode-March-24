bef5f602543b699f8162321649749694f23dcc84
harshfi <86000141+harshfi@users.noreply.github.com>
2024-07-30 23:12:52 +0530

    Time: 11 ms (34.02%) | Memory: 43.5 MB (61.14%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,48 +1,41 @@
 class Solution {
     public int calculate(String s) {
-        int len= s.length();
-        int sign=1;
-        int ans=0;
-        int currNo=0;
-
-        Stack<Integer> st= new Stack<>();
-
-        for(int i=0;i<len;i++){
-            char ch=s.charAt(i);
-
-            if(Character.isDigit(ch)){
-                currNo=s.charAt(i)-'0';
-                
-                while(i+1<len &&Character.isDigit(s.charAt(i+1))){
-                    currNo=currNo*10+(s.charAt(i+1)-'0');
-                    i++;
-                }
-
-                currNo=currNo*sign;
-                ans+=currNo;
-                currNo=0;
-                sign=1;
-
+       int len=s.length();
+       int ans=0;
+       int currNo=0;
+       int sign=1;
+       Stack<Integer> st=new Stack<>();
+       for(int i=0;i<len;i++){
+        char ch= s.charAt(i);
+        if(Character.isDigit(ch)){
+            currNo=(s.charAt(i)-'0');
+            while(i+1<len && Character.isDigit(s.charAt(i+1)) ){
+                currNo=currNo*10+(s.charAt(i+1)-'0');
+                i++;
             }
-            else if(ch=='+')sign=1;
-            else if(ch=='-') sign=-1;
-            else if(s.charAt(i)=='('){
-                st.push(ans);
-                st.push(sign);
-                ans=0;
-                sign=1;
+            currNo=currNo*sign;
+            ans+=currNo;
+            currNo=0;
+            sign=1;
+       }
 
-            } 
-            else if(ch==')'){
-               int pS=st.pop();
-               ans=pS*ans;
+       else if(ch=='+')sign=1;
+       else if(ch=='-')sign=-1;
+       else if(ch=='('){
+        st.push(ans);
+        st.push(sign);
+        ans=0;
+        sign=1;
+       }
+       else if(ch==')'){
+        int prevSign=st.pop();
+        ans=prevSign*ans;
+        int pA=st.pop();
+        ans=ans+pA;
+       }
+       
 
-               int pA=st.pop();
-               ans=ans+pA;
-            }
-
-        }
-        return ans;
-        
+    }
+    return ans;
     }
 }
