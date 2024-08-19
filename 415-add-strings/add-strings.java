074e381921884a26bc356c86cb0328b6577efa8d
harshfi <86000141+harshfi@users.noreply.github.com>
2024-08-20 00:18:49 +0530

    Time: 35 ms (5.97%) | Memory: 45.4 MB (7.09%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,40 @@
+class Solution {
+public static  String addStrings(String nums1, String nums2) {
+	        int i=nums1.length()-1;
+	       int  j=nums2.length()-1;
+	        int idx=0;
+	        int a[]=new int[(int)Math.pow(10,4)+2];
+	        int c=0;
+	        while(i>=0 && j>=0){
+	          int d= (nums1.charAt(i--)-'0')+(nums2.charAt(j--)-'0')+c;
+	          a[idx++]=  d%10;
+	          c=d/10;
+	          System.out.println(d);
+	        }
+
+	         while(i>=0){
+	            int d= (nums1.charAt(i--)-'0')+c;
+	             a[idx++]=  d%10;
+	             c=d/10;
+	        }
+
+	         while(j>=0){
+	          int d=(nums2.charAt(j--)-'0')+c;
+	          a[idx++]=  d%10;
+	          c=d/10;
+	        }
+	        if(c==1) a[idx++]=1;
+	        
+	        idx--;
+	       String sum="";
+	        int mul=1;
+	        i=0;
+	        while(idx>=0){
+	            sum+=(a[idx]);
+	            mul=mul*10;
+	           idx--; 
+	        }
+
+	        return sum;
+	    }
+}
