cb28c1e4d4e32a66f4f9f998aace646b5c4ffc2e
harshfi <86000141+harshfi@users.noreply.github.com>
2024-08-24 17:06:54 +0530

    Time: 10 ms (20.85%) | Memory: 45.6 MB (5.15%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -3,38 +3,30 @@ public static  String addStrings(String nums1, String nums2) {
 	        int i=nums1.length()-1;
 	       int  j=nums2.length()-1;
 	        int idx=0;
-	        int a[]=new int[(int)Math.pow(10,4)+2];
+	        String ans="";
 	        int c=0;
 	        while(i>=0 && j>=0){
 	          int d= (nums1.charAt(i--)-'0')+(nums2.charAt(j--)-'0')+c;
-	          a[idx++]=  d%10;
+	          ans=  d%10+ans;
 	          c=d/10;
 	        //   System.out.println(d);
 	        }
 
 	         while(i>=0){
 	            int d= (nums1.charAt(i--)-'0')+c;
-	             a[idx++]=  d%10;
+	              ans=  d%10+ans;
 	             c=d/10;
 	        }
 
 	         while(j>=0){
 	          int d=(nums2.charAt(j--)-'0')+c;
-	          a[idx++]=  d%10;
+	           ans=  d%10+ans;
 	          c=d/10;
 	        }
-	        if(c==1) a[idx++]=1;
-	        
-	        idx--;
-	      StringBuilder sb= new StringBuilder();
+	        if(c==1) ans=  1+ans;
 	       
-	        i=0;
-	        while(idx>=0){
-	            sb.append(a[idx]);
-	          
-	           idx--; 
-	        }
+	      
 
-	        return sb.toString();
+	        return ans;
 	    }
 }
