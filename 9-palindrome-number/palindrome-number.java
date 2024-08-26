1d76c99e1f95bdffca12428eb09afe419bc0778d
harshfi <86000141+harshfi@users.noreply.github.com>
2024-08-26 20:32:34 +0530

    Time: 5 ms (69.79%) | Memory: 44.6 MB (5.75%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,6 +3,11 @@ class Solution {
        
         int rev = 0;
         int temp = x;
+
+        if(rev < (Integer.MIN_VALUE)/10 || rev > (Integer.MAX_VALUE)/10){
+            return false;
+        }
+
         
         while(x>0){
             int rem = x % 10;
