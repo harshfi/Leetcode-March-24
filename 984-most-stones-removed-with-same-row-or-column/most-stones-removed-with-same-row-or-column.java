76eeb8a793f387948dccfbe5efb770915eeecd1b
harshfi <86000141+harshfi@users.noreply.github.com>
2024-08-29 21:30:00 +0530

    Time: 30 ms (40.45%) | Memory: 45 MB (49.07%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,44 +1,55 @@
 class Solution {
-   public int removeStones(int[][] s) {
-        
-    	 int n=s.length;
-         List <Integer> []  l= new List [n];
-         
-         for (int i = 0; i < l.length; i++) {
-			l[i]=new ArrayList<>();
-		}
 
-         for(int i=0;i<s.length;i++) {
-        	 for(int j=i+1;j<s.length;j++ ) {
-        		 if(s[i][0]==s[j][0] ||s[j][1]==s[i][1]) {
-        			 
-        			 l[i].add(j);
-        			 l[j].add(i);
-        		 }
-        	 }
-         }
-         
-         int count=0;
-         int v[]= new int [n];
-         
-         for (int i = 0; i < v.length; i++) {
-			
-        	 if(v[i]==0) {
-        		 dfs(l,v,i);
-        		 count++;
-        	 }
-		}
-         return n-count;
-      
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        UnionFind uf = new UnionFind(n);
+
+        // Populate uf by connecting stones that share the same row or column
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (
+                    stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]
+                ) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return n - uf.count;
     }
-    
-    private void dfs(List<Integer> []  l, int []v,int i) {
-    	v[i]=1;
-    	for (int  j : l[i]) {
-			
-    		if(v[j]==0) {
-    			dfs(l,v,j);
-    		}
-		}
+
+    // Union-Find data structure for tracking connected components
+    private class UnionFind {
+
+        int[] parent; // Array to track the parent of each node
+        int count; // Number of connected components
+
+        UnionFind(int n) {
+            parent = new int[n];
+            Arrays.fill(parent, -1); // Initialize all nodes as their own parent
+            count = n; // Initially, each stone is its own connected component
+        }
+
+        // Find the root of a node with path compression
+        int find(int node) {
+            if (parent[node] == -1) {
+                return node;
+            }
+            return parent[node] = find(parent[node]);
+        }
+
+        // Union two nodes, reducing the number of connected components
+        void union(int n1, int n2) {
+            int root1 = find(n1);
+            int root2 = find(n2);
+
+            if (root1 == root2) {
+                return; // If they are already in the same component, do nothing
+            }
+
+            // Merge the components and reduce the count of connected components
+            count--;
+            parent[root1] = root2;
+        }
     }
 }
