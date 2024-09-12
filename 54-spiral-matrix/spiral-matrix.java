302ead745b16ae0458cdadbc64bec8b19b7174ae
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-09-12 17:06:18 +0530

    Time: 8 ms (100.00%) | Memory: 42.3 MB (9.06%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,39 +1,38 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> list = new ArrayList();
+    public List<Integer> spiralOrder(int[][] a) {
+        List<Integer> l= new ArrayList<>();
         int top=0;
-        int bottom=matrix.length-1;
+        int bottom= a.length-1;
         int left=0;
-        int right=matrix[0].length-1;
-        while(top<=bottom && left<=right)
-        {
-        for(int i=left;i<=right;i++)
-        {
-            list.add(matrix[top][i]);
+        int right=a[0].length-1;
+        System.out.println(top+" "+bottom+" "+left+" "+right);
+        while( top<=bottom && left<= right){
+            
+            for(int i=left;i<=right;i++){
+                l.add(a[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                l.add(a[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+               for(int i=right;i>=left;i--){
+                l.add(a[bottom][i]);
+            }
+            }
+            
+            bottom--;
+            if(left<=right){
+           
+            for(int i=bottom ; i>=top;i--){
+                l.add(a[i][left]);
+               
+            }
+
+            left++;
+            }
         }
-        top++;
-        for(int i=top;i<=bottom;i++)
-        {
-            list.add(matrix[i][right]);
-        }
-        right--;
-        if(top<=bottom)
-        {
-        for(int i=right;i>=left;i--)
-        {
-            list.add(matrix[bottom][i]);
-        }
-        }
-        bottom--;
-        if(left<=right)
-        {
-        for(int i=bottom;i>=top;i--)
-        {
-            list.add(matrix[i][left]);
-        }
-        left++;  
-        }
-        }
-        return list;
+        return l;
     }
 }
