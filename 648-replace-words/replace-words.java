46a70e7b1db66dc436aab15372a1666f92b79771
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-09-16 02:33:53 +0530

    Time: 56 ms (44.46%) | Memory: 70.6 MB (5.01%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,29 +1,66 @@
 class Solution {
-    public String replaceWords(List<String> d, String se) {
-
-        Set<String> set=new HashSet<>();
-
-        for(String i: d)set.add(i);
-        String ss="";
-        String []s=se.split(" ");
-
-        StringBuilder sb=new StringBuilder();
-
-        for(String i: s){
-            int t=0;
-            for(int j=0;j<i.length();j++){
-                 String str=i.substring(0,j+1);
-
-                 if(set.contains(str)){
-                   t=1;
-                   sb.append(" "+str);
-                   break;
-                 }
-            }
-            if(t==0)sb.append(" "+i);
-        }
-        return sb.toString().trim();
-
+    class Node{
+		char data;
+		HashMap<Character,Node> child=new HashMap<>();
+		boolean isTerminal;
+		
+	}
+	
+	private Node root;
+	
+	public Solution(){
+		Node n= new Node();
+		root=n;
+		n.data='*';
+		n.isTerminal=false;
+	}
+	
+	public void insert(String word) {
+		
+		Node curr=root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch= word.charAt(i);
+			
+			if(curr.child.containsKey(ch)) {
+				curr=curr.child.get(ch);
+			}
+			else {
+				Node n=new Node();
+				n.data=ch;
+				curr.child.put(ch,n);
+				curr=n;
+			}
+		}
+		curr.isTerminal=true;
+	}
+	
+	public int Search(String word) {
+		Node curr=root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch= word.charAt(i);
+			
+            if(curr.isTerminal) return i;
+			if(curr.child.containsKey(ch)) {
+                // System.out.println("hi");
+				curr=curr.child.get(ch);
+			}
+			else {
+				return word.length();
+			}
+		}
+		return word.length();
+	}
+	
+    public String replaceWords(List<String> d, String st) {
+        String s[]=st.split(" ");
+        String str="";
         
+        for(String  i : d) insert(i);
+        for(String i : s){
+            int l=Search(i);
+            // System.out.println(i+" "+l);
+            str=str+i.substring(0,l)+" "; 
+        }
+        return str.trim();
     }
 }
