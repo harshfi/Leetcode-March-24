080149c9055ca3c7fb46b15c7c7f3046233fb5ae
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-09-28 10:15:59 +0530

    Time: 6 ms (25.74%) | Memory: 45.3 MB (6.94%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,65 +1,77 @@
 class Solution {
-    List<List<String>> list;
+    List<List<String>> ans;
     public List<List<String>> solveNQueens(int n) {
-        list=new ArrayList<>();
-        int board[][]=new int[n][n];
-        find(board,n,0);
-        return list;
-    }
-    public void find(int board[][],int n,int row){
-        if(n==0){
-            List<String> l=new ArrayList<>();
-            for(int i=0;i<board.length;i++){
-                String str="";
-                for(int j=0;j<board.length;j++){
-                    if(board[i][j]==1)str+="Q";
-                    else str+='.';
-                }
-                l.add(str);
-            }
-            list.add(new ArrayList<>(l));
-            return ;
 
-        }
-        for(int i=0;i<board.length;i++){
-            if(isSafe(board,row,i)){
-                board[row][i]=1;
-                find(board,n-1,row+1);
-                board[row][i]=0;
-            }
-        }
-    }
-    public boolean isSafe(int board[][],int row,int col){
-        //left dia
-        int r=row;
-        int c=col;
-        while(r>=0 && c>=0){
-            if(board[r][c]==1){
-                return false;
-            }
-            r--;
-            c--;
-        }
-        //right dia
-        r=row;
-        c=col;
-        while(r>=0 && c<board.length){
-            if(board[r][c]==1){
-                return false;
-            }
-            r--;
-            c++;
-        }
-        //vertical
-         r=row;
-        c=col;
-        while(r>=0){
-            if(board[r][c]==1){
-                return false;
-            }
-            r--;
+        ans= new ArrayList<>();
+        int board[][]= new int [n][n];
+		
+		find(0,board);
+        return ans;
 
-        }
-        return true;
     }
+    public void find(int row,int board[][] ) {
+		if(row==board.length) {
+			List<String> list= new ArrayList<>();
+            for (int i = 0; i < board.length; i++) {
+                String s="";
+			for (int j = 0; j < board.length; j++) {
+				if(board[i][j]==1) {
+					s=s+"Q";
+				}
+				else s=s+".";
+			}
+			list.add(s);
+		}
+        ans.add(new ArrayList<>(list));
+			return;
+		}
+		for (int col = 0; col < board[0].length; col++) {
+			
+			if(isSafe(row,col,board)==true) {
+				board[row][col]=1;
+				find(row+1,board);
+				board[row][col]=0;
+			}
+		}
+	}
+	public static boolean isSafe(int row,int col,int board[][]) {
+		int r=row;
+		int c=col;
+		//left dia
+		while(r>=0 && c>=0) {
+			if(board[r][c]==1) return false;
+			r--;
+			c--;
+			
+		}
+		r=row;
+		c=col;
+		//right dia
+		while(r>=0 && c<board.length) {
+			if(board[r][c]==1) return false;
+			r--;
+			c++;
+		}
+		//vertical
+		r=row;
+		c=col;
+		while(r>=0) {
+			if(board[r][c]==1) return false;
+			r--;
+		}
+		return true;
+		
+	}
+	static void print(int board[][]) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if(board[i][j]==1) {
+					System.out.print("Q  ");
+				}
+				else System.out.print("_ ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
