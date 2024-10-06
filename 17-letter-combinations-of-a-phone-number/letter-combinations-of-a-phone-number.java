124eaf41925106013469302788fd6ecbb7397497
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-10-06 12:24:53 +0530

    Time: 5 ms (33.67%) | Memory: 42.3 MB (30.02%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,22 +1,24 @@
 class Solution {
-    ArrayList<String> list;
-    public List<String> letterCombinations(String digits) {
-        String l[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        list=new ArrayList<>();
-        if(digits.length()==0) return list;
-        combination(l, "",digits,0);
-        return list;
-        }
-        public  void combination(String l[],String s,String digits, int idx){
-            if(digits.length()==idx) {
-                list.add(s);
-                return ;
-            }
-            String str = l[ digits.charAt(idx)-48];
-
-            for(int i=0;i<str.length();i++){
-               combination(l, s+str.charAt(i),digits,idx+1); 
-            }
-        }
+    static List<String> list;
+	public static List<String> letterCombinations(String digit) {
+		list=new ArrayList<>();
+        if(digit.length()==0) return list;
+		String arr[]={"","","abc","def","ghi",
+		              "jkl","mno","pqrs","tuv","wxyz"};
+		find("",0,arr,digit);
+		return list;
+	}
+	public static void find(String ans,
+			int idx,String arr[],String digit) {
+		if(ans.length()==digit.length()) {
+			list.add(ans);
+			return;
+		}
+		int d= digit.charAt(idx)-'0';
+		String s= arr[d];
+		for (int i = 0; i < s.length(); i++) {
+			find(ans+s.charAt(i),idx+1,arr,digit);
+		}
+	}
 
 }
