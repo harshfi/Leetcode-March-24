e5d3dd985ddbfb1e5aa0a5118c651f9bfaba9ce7
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-10-21 11:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (44.76%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] a) {
-        int n=a.length;
-        int max=0;
-        for(int i=0;i<a.length;i++){
-            int sum=0;
-            for(int j=0;j<a[i].length;j++){
-                sum=sum+a[i][j];
-            }
-            max=Math.max(sum,max);
-        }
-
-        return max;
+          
+          int max=0;
+		for (int i = 0; i < a.length; i++) {
+			int sum=0;
+			for (int j = 0; j < a[0].length; j++) {
+				sum=sum+a[i][j];
+			}
+			if(max<sum) max=sum;
+		}
+		return max;
+        
     }
 }
