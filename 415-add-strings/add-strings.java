68d845a06f1993d75a1c26ae2b378e1a33477911
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-10-22 11:54:06 +0530

    Time: 10 ms (24.39%) | Memory: 45.4 MB (8.06%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,31 +1,31 @@
 class Solution {
-public static  String addStrings(String num1, String num2) {
-	       int i=num1.length()-1;
-		int j=num2.length()-1;
-		String sum="";
+public static  String addStrings(String n1, String n2) {
+	    String ans="";
 		int c=0;
+		int i=n1.length()-1;
+		int j=n2.length()-1;
 		while(i>=0 && j>=0) {
-			
-			int d=(num1.charAt(i)-'0')+(num2.charAt(j)-'0')+c;
-			sum=(d%10)+sum;
+			int d= (n1.charAt(i)-'0')+(n2.charAt(j)-'0')+c;
+			ans=(d%10)+ans;
 			c=d/10;
 			i--;
-			j--;
-		}
-		while(j>=0) {
-			int d=(num2.charAt(j)-'0')+c;
-			sum=(d%10)+sum;
-			c=d/10;
 			j--;
 		}
 		
 		while(i>=0) {
-			int d=(num1.charAt(i)-'0')+c;
-			sum=(d%10)+sum;
+			int d= (n1.charAt(i)-'0')+c;
+			ans=(d%10)+ans;
 			c=d/10;
 			i--;
 		}
-		if(c>0)sum=c+sum;
-		return sum;
+		
+		while(j>=0) {
+			int d= (n2.charAt(j)-'0')+c;
+			ans=(d%10)+ans;
+			c=d/10;
+			j--;
+		}
+		if(c>0) ans=c+ans;
+        return ans;
 	    }
 }
