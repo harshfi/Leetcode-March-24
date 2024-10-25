53f06a909f01a06e4b69f85e392a9121d58c4440
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2024-10-25 12:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (42.23%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,13 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        long s=1;
-	    long e=x;
-	    while(s<=e) {
-	    	long mid=(s+e)/2;
-	    	if(mid*mid==x) return (int)mid;
-	    	else if(mid*mid>x) e=mid-1;
-	    	else s=mid+1;
-	    }
-	    return (int)e;
+         return (int) Math.sqrt(x);
     }
 }
