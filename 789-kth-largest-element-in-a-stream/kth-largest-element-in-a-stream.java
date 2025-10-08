8b426df67879b915227fc7efc84bfa6bc1c19010
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2025-10-08 19:05:07 +0530

    Time: 19 ms (96.81%) | Memory: 50.1 MB (54.59%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,37 +1,25 @@
 class KthLargest {
- PriorityQueue<Integer> pq  ;
- int k;
-	public KthLargest(int k, int[] nums) {
+    PriorityQueue<Integer> pq= new PriorityQueue<>();
+    int k;
+    public KthLargest(int k, int[] arr) {
         this.k=k;
-		pq= new PriorityQueue<>(); 
-		for(int i=0;i<k && i<nums.length;i++) {
-			pq.add(nums[i]);
-		}
-		for (int i = k; i < nums.length; i++) {
-			
-			if(nums[i]>pq.peek()) {
-				pq.remove();
-				pq.add(nums[i]);
-				}
-		}
-		
-		 
-	    }
-	    
-	    public int add(int val) {
-            if(pq.size()<k){
-                pq.add(val);
-                return pq.peek();
-
-            } 
-	    	if(val>pq.peek()) {
-				pq.remove();
-				pq.add(val);
-				
-				}
-	    	return pq.peek();
-	        
-	    }
+        for(int i=0;i<arr.length;i++){
+            if(pq.size()<k)pq.add(arr[i]);
+            else if(pq.size()==k && pq.peek()<arr[i]){
+                  pq.remove();
+                  pq.add(arr[i]);
+            }
+        }
+    }
+    
+    public int add(int val) {
+        if(pq.size()<k)pq.add(val);
+         else  if(pq.size()==k && pq.peek()<val){
+                  pq.remove();
+                  pq.add(val);
+            }
+            return pq.peek();
+    }
 }
 
 /**
