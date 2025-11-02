3bfad8a80ac68a1520a7fb829f589d23b77f43f3
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2025-11-03 01:44:22 +0530

    Time: 6 ms (39.51%) | Memory: 44.8 MB (99.76%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,39 +1,32 @@
 class Solution {
-    
     public int minimumTotal(List<List<Integer>> t) {
-        int dp[][]=new int[t.size()][];
-        for(int i=0;i<t.size();i++){
-            dp[i]=new int[t.get(i).size()+1];
-            Arrays.fill(dp[i],1000000);
-        }
-       int x=find(t,0,0, dp);
-    //    print(dp);
-       return x; 
-    }
-    public void print(int dp[][]){
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[i].length;j++){
-                System.out.print(dp[i][j]+" ");
+        if (t == null || t.size() == 0) return 0;
+
+        int dp[][] = new int[t.size()][];
+
+        dp[0] = new int[1];
+        dp[0][0] = t.get(0).get(0);
+
+        for (int i = 1; i < t.size(); i++) {
+            dp[i] = new int[t.get(i).size()];
+            // leftmost
+            dp[i][0] = dp[i-1][0] + t.get(i).get(0);
+
+            for (int j = 1; j < t.get(i).size(); j++) {
+                int val = t.get(i).get(j);
+                if (j == t.get(i).size() - 1) {
+                    // rightmost: only parent is dp[i-1][j-1]
+                    dp[i][j] = dp[i-1][j-1] + val;
+                } else {
+                    // middle: parents dp[i-1][j-1] and dp[i-1][j]
+                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + val;
+                }
             }
-            System.out.println();
         }
-    }
-    public int find(List<List<Integer>> t,int r, int c, int[][] dp){
-        if(r==t.size()){
-          
-           return 0;
-        }
-        if(dp[r][c]!=1000000)return dp[r][c];
-      int v1=100000, v2=100000;
-      if(c<t.get(r).size())v1=t.get(r).get(c)+ find(t,r+1,c,dp);
-      if(c+1<t.get(r).size())v2=t.get(r).get(c+1) +find(t,r+1,c+1, dp);
 
-     if(c<dp[r].length)dp[r][c]=Math.min(v1,v2);
-    //  if(c+1<dp[r].length)dp[r][c+1]=Math.min(v1,v2);
-
-     return Math.min(v1,v2);
-
-
-
+        int last = t.size() - 1;
+        int ans = Integer.MAX_VALUE;
+        for (int x : dp[last]) ans = Math.min(ans, x);
+        return ans;
     }
 }
