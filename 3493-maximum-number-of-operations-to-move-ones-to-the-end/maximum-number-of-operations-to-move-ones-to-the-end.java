0a13e610ea980ed0d81bfe23afec4da295046910
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2025-11-13 16:38:38 +0530

    Time: 7 ms (78.52%) | Memory: 47.2 MB (25.93%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,16 +1,18 @@
 class Solution {
     public int maxOperations(String s) {
-        int ans = 0;
-        int cnt = 0;               // number of '1's seen so far
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            if (s.charAt(i) == '1') {
-                ++cnt;
-            } else if (i > 0 && s.charAt(i - 1) == '1') {
-                // first '0' after a block of '1's:
-                ans += cnt;
+        int ans=0;
+        
+        int cnt=0;
+
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)=='1'){
+                cnt++;
+
             }
+            else if(i>0 && s.charAt(i-1)=='1')ans=ans+cnt;
         }
         return ans;
+
     }
 }
