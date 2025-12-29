bf6bdb4f9f13eb17f59d23e23a195038bc5d332b
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2025-12-30 00:08:52 +0530

    Time: 10 ms (99.58%) | Memory: 46.4 MB (45.55%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int a = coin; a <= amount; a++) {
+                dp[a] = Math.min(dp[a], dp[a - coin] + 1);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
