f554605312bba774f0f6f56c4f69ba85465293c2
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-01-04 02:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (71.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int a[]= new int[2];
-        a[0]=LowerBound(nums,target);
-        a[1]=UpperBound(nums,target);
-        return a;
+        int lb=lb(nums,target);
+        int ub=ub(nums,target);
+        int arr[]= new int[2];
+        arr[0]=lb;
+        arr[1]=ub;
+        return arr;
     }
-    public static int LowerBound(int nums[], int target){
-        // if(nums.length>=1 && nums[0]>target) return -1;
-
-        int i=0,e=nums.length-1;
-        int ans=-1;
-        while(i<=e){
-            int mid=i+(e-i)/2;
-
-            if(nums[mid]==target){
-                ans=mid;
-                e=mid-1;
-            }
-            else if(nums[mid]> target) e=mid-1;
-            else i= mid+1;
-        }
-        return ans;
+    public int ub(int []nums, int target){
+        int i=0, j=nums.length-1;
+       int ans=-1;
+       while(i<=j){
+          int mid=(i+j)/2;
+          if(nums[mid]<=target){
+            if(nums[mid]==target) ans=mid;
+           
+            i=mid+1;
+          }
+          else j=mid-1;
+       }
+       return ans;
     }
-    public static int UpperBound(int nums[], int target){
-        // if(nums.length>=1 &&nums[nums.length-1]<target) return -1;
-
-        int i=0,e=nums.length-1;
-        int ans=-1;
-        while(i<=e){
-            int mid=i+(e-i)/2;
-
-            if(nums[mid]==target){
-                ans=mid;
-                i=mid+1;
-            }
-            else if(nums[mid]> target) e=mid-1;
-            else i= mid+1;
-        }
-        return ans;
+    public int lb(int[] nums, int target){
+       
+       int i=0, j=nums.length-1;
+       int ans=-1;
+       while(i<=j){
+          int mid=(i+j)/2;
+          if(nums[mid]>=target){
+            if(nums[mid]==target) ans=mid;
+           
+            j=mid-1;
+          }
+          else i=mid+1;
+       }
+       return ans;
     }
 }
