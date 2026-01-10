b2b69c14cde24478e4c11ef83cd3d316ecde0e73
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-01-10 14:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (73.02%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,30 +1,37 @@
 class Solution {
-    int[][] dp;
+    public int splitArray(int[] arr, int k) {
+        int max=arr[0];
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            max=Math.max(max,arr[i]);
+            sum+=arr[i];
+        }
 
-    public int splitArray(int[] nums, int k) {
-        dp = new int[nums.length][k + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return dfs(nums, 0, k);
+        int i=max;
+        int j=sum;
+        int ans=sum;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(isCorrect(arr,mid,k)){
+                ans=mid;
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return ans;
+        
     }
-
-    int dfs(int[] nums, int i, int k) {
-        if (k == 1) {
-            int sum = 0;
-            for (int x = i; x < nums.length; x++) sum += nums[x];
-            return sum;
+    public static boolean isCorrect(int []arr,int mid,int k){
+        int count=1;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum>mid){
+                sum=arr[i];
+                count++;
+            }
         }
-
-        if (dp[i][k] != -1) return dp[i][k];
-
-        int sum = 0;
-        int res = Integer.MAX_VALUE;
-
-        for (int j = i; j <= nums.length - k; j++) {
-            sum += nums[j];
-            int next = dfs(nums, j + 1, k - 1);
-            res = Math.min(res, Math.max(sum, next));
-        }
-
-        return dp[i][k] = res;
+        return count<=k;
     }
 }
