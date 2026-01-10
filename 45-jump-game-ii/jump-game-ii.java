63151c111dc5ff31b97eef11bee6357ed1c92519
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-01-10 23:09:02 +0530

    Time: 1 ms (99.27%) | Memory: 47.3 MB (59.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,36 +1,23 @@
 class Solution {
-static  int min=1000000;
-	    public static int jump(int[] nums) {
-             min=1000000;
-             int dp[]=new int [nums.length];
-             Arrays.fill(dp,1000000);
-	         if(nums.length==1)return 0;
-	         
-	       
-	        	return find(nums,0,dp);
-	        
-	        
-	        // return min;
-	    }
-	    public static int  find(int []nums,int i,int dp[]){
-	        if(i>=nums.length-1){
-	        //    min=Math.min(min,c);
-	           return 0;
-	        }
-	        if(nums[i]==0)return 1000000 ;
-
-            if(dp[i]!=1000000)return dp[i];
-
-	        for(int j=1+i;j<=nums.length && j<=nums[i]+i;j++){
-
-	          int c=1+find(nums,j,dp);
-              dp[i]=Math.min(c,dp[i]);
-	        }
-            return dp[i];
-
-
-	    }
-
-
-
+    public int jump(int[] a)
+    {
+       
+        int cm=0;
+        int cr=0;
+        int jump=0;
+        for(int i=0;i<a.length-1;i++)
+        {
+            if(i+a[i]>cm)
+            {
+                cm=i+a[i];
+            }
+            if(i==cr)
+            {
+                jump++;
+                cr=cm;
+            }
+        }
+       
+        return jump;
+    }
 }
