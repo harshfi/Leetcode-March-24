92e350f2acd8d407d47115636b7d8b3c63692f98
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-01-22 23:33:24 +0530

    Time: 6 ms (62.30%) | Memory: 46.3 MB (37.03%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,43 +1,24 @@
 class Solution {
-     Set<String> set;
-     boolean ans=false;
-     int dp[];
-    public boolean wordBreak(String s, List<String> word) {
-       set =new  HashSet<>();
-       dp=new int [s.length()+1];
-        for(String i: word) set.add(i);
-       Queue<Integer> q= new LinkedList<>();
-       q.add(0);
-       while(!q.isEmpty()){
-        int i= q.remove();
-        if(i==s.length())return true;
+    int dp[];
+    public boolean wordBreak(String s, List<String> list) {
+       dp= new int [s.length()+1];
 
-        for(int j=i+1;j<=s.length();j++){
-            if(dp[j]==1)continue;
+        return solve(s,list,  0);
+    }
+    
+    boolean solve(String s, List<String> list, int i){
+      if(s.length()==i){ return true; } 
+      if(dp[i]==1)return false;
+      for( int j=i;j<s.length();j++){
+         if(list.contains(s.substring(i,j+1)))
+            {
+                 boolean check=solve(s,list,j+1);
+                 if(check==true)return true;
+           } 
+           }
 
-            if(set.contains(s.substring(i,j))){
-                dp[j]=1;
-                q.add(j);
-            }
-        }
-       }
-        return false;
+           dp[i]=1;
+           return false;
     }
-  boolean  find(String s,int i){
-        if(s.length()==i){
-            ans=true;
-            return true;
-        }
-        if(i>=s.length())return false;
-        if(dp[i]!=0)return true;
-        for( int j=i ;j<s.length();j++){
-            String str=s.substring(i,j+1);
-            if(set.contains(str)){
-            //   System.out.println(str +" "+s.substring(j+1,s.length()));
-              dp[i]= find(s,j+1) ?1:0;
-                if(ans)return true;
-            }
-        }
-        return dp[i]==1?true:false;
-    }
+    
 }
