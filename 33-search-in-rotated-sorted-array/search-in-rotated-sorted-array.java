f5f06d17d256065fa4d33bee505f43d732702b49
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-02-23 22:21:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (82.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,45 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int idx=peek(nums);
-        int ans=search(target,nums,0,idx);
-        System.out.println(ans+" "+idx);
-        if(ans!=-1)return ans;
-        else return search(target,nums,idx+1,nums.length-1);
-     
-        
-    }
-    public int search(int target, int nums[], int i, int j){
-         
-        while(i<=j){
-             int mid=(i+j)/2;
+        int s=0;
+        int e=nums.length-1;
 
-             if(nums[mid]==target){
-                return mid;
-             }
-             else if(nums[mid]<target){
-                i=mid+1;
-             }
-             else{
-               j=mid-1;
-             }
-        
+        while(s<=e){
+            int mid=(s+e)/2;
+
+            if(nums[mid]==target)return mid;
+
+            if(nums[mid]>=nums[s]){
+
+                if(target>=nums[s] && target<= nums[mid]){
+                    e=mid-1;
+                }
+                else s=mid+1;
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[e])s=mid+1;
+                else e=mid-1;
+            }
         }
         return -1;
-    }
-    public int peek(int nums[]){
-        // 
-        int i = 0, j = nums.length - 1;
-
-    while (i < j) {
-        int mid = (i + j) / 2;
-
-        if (nums[mid] > nums[j]) {
-            i = mid + 1;
-        } else {
-            j = mid;
-        }
-    }
-    return (i - 1 + nums.length) % nums.length;
     }
 }
