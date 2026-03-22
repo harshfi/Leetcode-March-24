71f4771d318080e21e3ef41adc355989df8e9a83
harsh tripathi <86000141+harshfi@users.noreply.github.com>
2026-03-22 23:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (65.92%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,30 +1,43 @@
 class Solution {
-    public void rotate(int[][] a) {
-        int n=a.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                int temp=a[i][j];
-                a[i][j]=a[j][i];
-                a[j][i]=temp;
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        int urow = 0, lrow = n - 1, lcol = 0, rcol = n - 1;
+
+        int[][] dp = new int[n][n];
+
+        while (urow <= lrow && lcol <= rcol) {
+
+            // top row → right column
+            for (int i = lcol; i <= rcol; i++) {
+                dp[i][rcol] = mat[urow][i];
             }
+
+            // right column → bottom row
+            for (int i = urow + 1; i <= lrow; i++) {
+                dp[lrow][rcol - (i - urow)] = mat[i][rcol];
+            }
+
+            // bottom row → left column
+            for (int i = rcol - 1; i >= lcol; i--) {
+                dp[lrow - (rcol - i)][lcol] = mat[lrow][i];
+            }
+
+            // left column → top row
+            for (int i = lrow - 1; i > urow; i--) {
+                dp[urow][lcol + (lrow - i)] = mat[i][lcol];
+            }
+
+            urow++;
+            lrow--;
+            lcol++;
+            rcol--;
         }
 
-       
-
-        
-
-        for(int i=0;i<n;i++){
-            int s=0;
-            int e=n-1;
-            while(s<e){
-                int temp=a[i][s];
-                a[i][s]=a[i][e];
-                a[i][e]=temp;
-                s++;
-                e--;
+        // copy back to original matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = dp[i][j];
             }
         }
-        
     }
-    
 }
